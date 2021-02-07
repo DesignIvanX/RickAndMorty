c8ff6302396c3e5836628f6a2a8189c874cc66f2
DesignIvanX <ivandcdesign@gmail.com>
2021-02-07 01:44:03 +0100

    App v1.5

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/home">
+          <a href="#/home/">
             Rick And Morty
           </a>
         </h1>
